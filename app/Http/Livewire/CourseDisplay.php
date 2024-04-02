106ddcc1d82e612e8138135d13284a40a41c897a
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2024-04-02 16:12:43 +0500

    init

---
+++ app/Http/Livewire/CourseDisplay.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Livewire;
+use Livewire\Component;
+
+class CourseDisplay extends Component
+{
+    public function render()
+    {
+        return view('livewire.course-display');
+    }
+}
